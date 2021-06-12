c490af4eadfebef19671b2f01c770810cb7065b5
Matheus Santos <matheus.ads.study@mail.com>
2021-06-12 12:36:41 -0300

    (Style) Global

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
+import '../theme.dart';
+
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Slidy',
-      theme: ThemeData(primarySwatch: Colors.blue),
+      debugShowCheckedModeBanner: false,
+      title: "Course Purchase",
+      theme:theme(),
     ).modular();
   }
 }
---
+++ lib/size_config.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class SizeConfig {
+  static MediaQueryData _mediaQueryData= new MediaQueryData();
+  static double screenWidth = 0;
+  static double screenHeight = 0;
+  static double defaultSize = 0;
+
+  void init(BuildContext context) {
+    _mediaQueryData = MediaQuery.of(context);
+    screenWidth = _mediaQueryData.size.width;
+    screenHeight = _mediaQueryData.size.height;
+  }
+}
+
+double getProportionateScreenHeight(double inputHeight) {
+  double screenHeight = SizeConfig.screenHeight;
+  return (inputHeight / 812.0) * screenHeight;
+}
+
+double getProportionateScreenWidth(double inputWidth) {
+  double screenWidth = SizeConfig.screenWidth;
+  return (inputWidth / 375.0) * screenWidth;
+}
